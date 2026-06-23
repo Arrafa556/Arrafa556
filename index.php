1a753603c8295805b2585de37ff2aba4b3b25310
ArrafaMa19 <arrafaaddress@gmail.com>
2026-06-23 18:56:14 +0700

    Uplod File folder Belajar PHP
    23/6/2026

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    $name = "Arrafa";
+    $umur = 16;
+?>
+<!DOCTYPE html >
+<html>
+<head>
+    <title>Belajar Php<title>
+</head>
+<body>
+    <h1>Halo, <?php echo $name; ?></h1>
+    <h2>halo, <?php echo $umur; ?></h2>
+</body>
+        </html>
